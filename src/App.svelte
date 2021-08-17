ac31ad101605337599358e255dd3f37ea3a6bd41
3nt3 <gott@3nt3.de>
2021-08-17 08:38:58 +0200

    add some interface

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,84 @@
 <script>
-	export let name;
+    import { setupI18n } from './i18n';
+
+    setupI18n({ withLocale: 'en' });
+
+    function getClassesForWeekday(weekday) {
+        const today = new Date().getDay();
+
+        if (weekday === today) {
+            return 'today';
+        } else if (weekday === today + 1) {
+            return 'tomorrow';
+        } else if (weekday === today + 2) {
+            return 'tdat';
+        } else if (weekday < today) {
+            return 'past';
+        } else {
+            return 'future';
+        }
+    }
+
+    export const days = [
+        'monday',
+        'tuesday',
+        'wednesday',
+        'thursday',
+        'friday',
+    ];
+
+    import { _ } from 'svelte-i18n';
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div id="week-container">
+    <h1>{$_('next3Days')}</h1>
+    <div id="week-row">
+        {#each [1, 2, 3, 4, 5] as day}
+            <div class="day">
+                <h2 class={getClassesForWeekday(day)}>{$_(days[day - 1])}</h2>
+            </div>
+        {/each}
+    </div>
+</div>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    h1 {
+        font-size: 22pt;
+    }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    h2 {
+        font-size: 14pt;
+    }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    #week-row {
+        display: flex;
+        /* TODO: responsiveness */
+    }
+
+    #week-row div {
+        width: 100%;
+        /* color: white; */
+        text-align: center;
+    }
+
+    .day h2 {
+        text-decoration: underline;
+        text-decoration-thickness: 4pt;
+        text-decoration-color: #d1ccc0;
+        text-underline-offset: 6px;
+    }
+    .today {
+        text-decoration-color: #ff5252 !important;
+    }
+    .tomorrow {
+        text-decoration-color: #ff793f !important;
+    }
+
+    .tdat {
+        text-decoration-color: #ffb142 !important;
+    }
+
+    .future {
+        text-decoration-color: #40407a !important;
+    }
 </style>
---
+++ src/i18n.js
@@ -0,0 +1,33 @@
+import { get } from 'svelte/store';
+import { addMessages, locale, init, dictionary, _ } from 'svelte-i18n';
+
+const MESSAGE_FILE_URL_TEMPLATE = '/locales/{locale}.json';
+function setupI18n(options) {
+    const { withLocale: locale_ } = options;
+    // Initialize svelte-i18n
+    init({ initialLocale: locale_ });
+    // Don't re-download translation files
+    if (!hasLoadedLocale(locale_)) {
+        const messagesFileUrl = MESSAGE_FILE_URL_TEMPLATE.replace(
+            '{locale}',
+            locale_,
+        );
+        // Download translation file for given locale/language
+        return loadJson(messagesFileUrl).then((messages) => {
+            // Configure svelte-i18n to use the locale
+            addMessages(locale_, messages);
+            locale.set(locale_);
+        });
+    }
+}
+function loadJson(url) {
+    return fetch(url).then((response) => response.json());
+}
+function hasLoadedLocale(locale) {
+    // If the svelte-i18n dictionary has an entry for the
+    // locale, then the locale has already been added
+    return get(dictionary)[locale];
+}
+// We expose the svelte-i18n _ store so that our app has
+// a single API for i18n
+export { _, setupI18n };
